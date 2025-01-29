4893a07bb6a859f12d8f889af812bfb652fcfd55
Ryan Paulista <ryanpaulista2610@gmail.com>
2025-01-29 14:49:04 -0300

    modificações cliente

--- cliente.js
+++ cliente.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const net = require('net');
 const si = require('systeminformation');
 
@@ -5,9 +6,17 @@ const client = new net.Socket();
 
 client.connect(3000, '127.0.0.1', async () => {
     console.log('Conectado ao servidor');
-
-    const cpuTemp = await si.cpuTemperature();
-    client.write(`Temperatura da CPU: ${cpuTemp.main}°C`);
-
+    try{
+        const memInfo = await si.mem();
+        const diskInfo = await si.fsSize();
+        const disk1 = diskInfo[0];
+        client.write(`Mémoria RAM livre: ${memInfo.free}\n`);
+        client.write(`Espaço em disco livre: ${disk1.available}`);
+    } catch {
+        console.error('Erro ao obter informações de memória: ', error);
+    } finally{
+        client.end();
+    }
+    
     client.end();
 });
