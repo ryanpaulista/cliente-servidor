517934a9fb11b572ca02be0d20b7e003077d02dd
Ryan Paulista <162994389+ryanpaulista@users.noreply.github.com>
2025-02-05 10:24:05 -0300

    Add files via upload

---
+++ Cliente.js
@@ -0,0 +1,13 @@
+class Cliente{
+
+    constructor(id, mem, diskFree, temp, cores){
+        this.id = id;
+        this.mem = mem;
+        this.diskFree = diskFree;
+        this.temp = temp;
+        this.cores = cores;
+    }
+
+
+    
+}
