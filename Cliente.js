30d1d96be1bddbaf24ef4a29ea33af1926ec399c
Ryan Paulista <162994389+ryanpaulista@users.noreply.github.com>
2025-02-11 09:00:54 -0300

    cliente para teste de conexão

--- Cliente.js
+++ Cliente.js
@@ -1,13 +1,30 @@
-class Cliente{
+const dgram = require("dgram");
+const os = require("os");
 
-    constructor(id, mem, diskFree, temp, cores){
-        this.id = id;
-        this.mem = mem;
-        this.diskFree = diskFree;
-        this.temp = temp;
-        this.cores = cores;
-    }
+const BROADCAST_ADDR = "255.255.255.255";
+const PORT = 41234;
+const INTERVAL = 10000; // Enviar a cada 5 segundos
 
+const client = dgram.createSocket("udp4");
+client.bind(() => {
+    client.setBroadcast(true);
+});
 
-    
+function getSystemInfo() {
+    return JSON.stringify({
+        hostname: os.hostname(),
+        freeMemory: os.freemem(),
+        totalMemory: os.totalmem(),
+        cpuCores: os.cpus().length,
+        loadAvg: os.loadavg(),
+        diskFree: "N/A", // Para pegar info de disco, seria necessário uma lib como 'diskusage'
+    });
 }
+
+setInterval(() => {
+    const message = Buffer.from(getSystemInfo());
+    client.send(message, 0, message.length, PORT, BROADCAST_ADDR, (err) => {
+        if (err) console.error("Erro ao enviar:", err);
+    });
+}, INTERVAL);
+
