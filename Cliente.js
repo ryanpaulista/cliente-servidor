4831b14788f86ca0dd3f175b4b814807d1da319a
Ryan Paulista <162994389+ryanpaulista@users.noreply.github.com>
2025-02-12 11:43:17 -0300

    Add files via upload

--- Cliente.js
+++ Cliente.js
@@ -2,9 +2,9 @@ const dgram = require("dgram");
 const os = require("os");
 const si = require("systeminformation");
 
-const BROADCAST_ADDR = "10.25.255.255"; // Troque pelo IP correto, ex: "172.25.111.255"
+const BROADCAST_ADDR = "10.25.255.255"; // 
 const PORT = 41234;
-const INTERVAL = 10000; // Enviar a cada 10 segundos
+const INTERVAL = 15000; // Enviar a cada 10 segundos
 
 const client = dgram.createSocket("udp4");
 client.bind(() => {
@@ -14,12 +14,12 @@ client.bind(() => {
 async function getSystemInfo() {
     try {
         const diskInfo = await si.fsSize(); // Aguarda os dados do disco
-        const diskFreeGB = (diskInfo[0].available / (1024 ** 3)).toFixed(2);
+        const diskFreeGB = diskInfo[0].available;
         return JSON.stringify({
             hostname: os.userInfo().username,
-            freeMemory: (os.freemem() / (1024 ** 3)).toFixed(2),
+            freeMemory: os.freemem(),
             cpuCores: os.cpus().length,
-            diskFree: diskFreeGB, // Pega espaço disponível no primeiro disco (diskInfo[0].available / (1024 ** 3)).toFixed(2)
+            diskFree: diskFreeGB, // Pega espaço disponível no primeiro disco, geralmente o disco C
         });
     } catch (error) {
         console.error("Erro ao obter informações do sistema:", error);
--- Servidor.js
+++ Servidor.js
@@ -3,15 +3,40 @@ const fs = require("fs");
 
 const PORT = 41234;
 const server = dgram.createSocket("udp4");
+const FILE_PATH = "clientes.json";
+const TIMEOUT = 30000;
+
+let clients = {};
+
+function saveClients(){
+    fs.writeFileSync(FILE_PATH, JSON.stringify(clients, null, 4));
+}
+
+function loadClients(){
+    if(fs.existsSync(FILE_PATH)){
+        try{
+            clients = JSON.parse((fs.readFileSync(FILE_PATH, "utf8")));
+        } catch(error){
+            console.error("Erro ao carregar arquivo JSON: ", error);
+        }
+    }
+}
+
+loadClients();
 
 server.on("message", (msg, rinfo) => {
-    const data = msg.toString();
-    console.log(`Recebido de ${rinfo.address}:`, data);
+    const clientID = rinfo.address;  
+    const data = JSON.parse(msg.toString());
     
+    clients[clientID] = {
+        ...data,
+        lastUpdate: Date.now(),
+    }
+
+    console.log(`Informações recebidas de ${clientID}`);
+
     // Salvar em um arquivo JSON
-    fs.appendFile("logs.json", rinfo.address + " = " + data + "\n", (err) => {
-        if (err) console.error("Erro ao salvar:", err);
-    });
+    saveClients();
 });
 
 server.on("listening", () => {
@@ -23,3 +48,22 @@ server.on("listening", () => {
 server.bind(PORT, "0.0.0.0", () => {
     server.setBroadcast(true); // Permite receber mensagens de broadcast
 });
+
+setInterval(() => {
+
+    const now = Date.now();
+    let updated = false;
+
+    for (const client in clients){
+        if(now - clients[client].lastUpdate > TIMEOUT){
+            console.log(`Removendo cliente inativo: ${client}`);
+            delete clients[client];
+            updated = true;
+        }
+    }
+
+    if (updated){
+        saveClients();
+    }
+
+}, TIMEOUT/2)
